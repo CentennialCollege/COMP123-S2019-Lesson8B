d17f2edac5c913a105d85ee6965d2041094b907a
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2019-06-26 15:21:18 -0400

    Created Base form for Lab 8

--- COMP123-S2019-Lesson8B/Lab08Form.Designer.cs
+++ COMP123-S2019-Lesson8B/Lab08Form.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace COMP123_S2019_Lesson8B
 {
-    partial class Form1
+    partial class Lab08Form
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Lab08Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(618, 424);
+            this.Name = "Lab08Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lab 8";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
