c012197aab0bbeb3b7979f2c3fc2815ac680fac4
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2019-06-26 15:27:30 -0400

    Added TitleLabel

--- COMP123-S2019-Lesson8B/Lab08Form.Designer.cs
+++ COMP123-S2019-Lesson8B/Lab08Form.Designer.cs
@@ -28,7 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.TitleLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLabel.Location = new System.Drawing.Point(13, 13);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(159, 55);
+            this.TitleLabel.TabIndex = 0;
+            this.TitleLabel.Text = "Lab08";
             // 
             // Lab08Form
             // 
@@ -36,14 +47,18 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
             this.ClientSize = new System.Drawing.Size(618, 424);
+            this.Controls.Add(this.TitleLabel);
             this.Name = "Lab08Form";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Lab 8";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label TitleLabel;
     }
 }
 
